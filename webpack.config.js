0455a26ac892915ca6706f28cc1f972e402025ff
LorenaGomezPerez <lorenagomezperez@gmail.com>
2023-08-14 14:44:06 +0200

    proof compilation with webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./components/add-bibliography.js",
+  output: {
+    path: __dirname + "/docs",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "images", to: "images" }],
+    }),
+  ],
+};
